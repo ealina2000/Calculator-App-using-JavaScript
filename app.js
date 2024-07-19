9c4b635cc296202ffe269aa7ebc09729374df16a
ealina2000 <kunduealina@gmail.com>
2024-07-19 17:37:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// Accessing the elements from html:-
+let buttons = document.querySelectorAll("button")
+let inp = document.querySelector("#input")
+let str = '';
+// Create buttons to an array by array.from methord
+let arr = Array.from(buttons);
+
+arr.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    // target "=" button
+    if (e.target.innerHTML === "=") {
+      str = eval(str);
+      inp.value = str;
+    } else if (e.target.innerHTML === "DEL") {
+      str = str.substring(0, str.length - 1)// If "DEL" button is clicked remeve the last character from input
+      inp.value = str;
+    } else if (e.target.innerHTML === "AC") {
+      str = "";//if "AC" button is clicked clear the input
+      inp.value = str;
+    }
+    // add evements to string
+    else {
+      str += e.target.innerHTML;
+      inp.value = str;
+    }
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
